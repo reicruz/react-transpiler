c374be00a9ea6781f11370672079ddde6dcefe72
Reinaldo Cruz <reinaldo422@gmail.com>
2016-10-09 17:05:12 -0400

    update path

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Transpiler from './components/Transpiler'
+import Transpiler from './Transpiler'
 
 ReactDOM.render(<Transpiler />, document.getElementById('app'));
